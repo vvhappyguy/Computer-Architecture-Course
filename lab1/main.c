4b399d5eddcc5a106a4c858e149f0011a1703aaf
vvhappyguy <vvhappyguy@gmail.com>
2019-09-25 16:20:41 +0300

    Added simple fork with output in all processes

--- lab1/main.c
+++ lab1/main.c
@@ -3,6 +3,20 @@
 
 int main()
 {
-    printf("PPID: %d\nPID: %d\n",getppid(), getpid());
+    printf("1: \n\tPPID: %d\n\tPID: %d\n",getppid(), getpid());
+    
+
+    if(fork() == 0)
+    {
+        // Parent pid
+        printf("2: \n\tPPID: %d\n\tPID: %d\n",getppid(), getpid());
+    }
+    else
+    {
+        // Forked pid
+        printf("2: \n\tPPID: %d\n\tPID: %d\n",getppid(), getpid());
+    }
+    
+    getchar();
     return 0;
 }
