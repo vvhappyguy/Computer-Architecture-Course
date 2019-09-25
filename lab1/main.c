a897be99419e434f5da6dd6e8b7f203d0c8628cc
vvhappyguy <vvhappyguy@gmail.com>
2019-09-25 16:21:37 +0300

    Added description and clang info

--- lab1/main.c
+++ lab1/main.c
@@ -1,3 +1,14 @@
+// Use Makefile for compiling
+// clang version 7.0.0-3 (tags/RELEASE_700/final)
+// Target: x86_64-pc-linux-gnu
+// Thread model: posix
+// InstalledDir: /usr/bin
+// Found candidate GCC installation: /usr/bin/../lib/gcc/x86_64-linux-gnu/8
+// Found candidate GCC installation: /usr/lib/gcc/x86_64-linux-gnu/8
+// Selected GCC installation: /usr/bin/../lib/gcc/x86_64-linux-gnu/8
+// Candidate multilib: .;@m64
+// Selected multilib: .;@m64
+
 #include <stdio.h>
 #include <unistd.h>
 
