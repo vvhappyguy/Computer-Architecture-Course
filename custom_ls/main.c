21ebb89e3645642116d1ff01c3bbaf005d009fd6
vvhappyguy <vvhappyguy@gmail.com>
2019-10-19 23:31:41 +0300

    Added TODO and total 16 drwxrwxr-x 2 dev dev 4096 окт 19 23:26 . drwxrwxr-x 5 dev dev 4096 окт  6 21:09 .. -rw-rw-r-- 1 dev dev 1459 окт 19 23:30 main.c -rw-rw-r-- 1 dev dev   69 окт  6 21:09 Makefile output format

--- custom_ls/main.c
+++ custom_ls/main.c
@@ -26,7 +26,8 @@ int main(void)
     
     
     // Prints dirent fields: $type\t$inode\t$size\t$name
-    while (n--) {
+    while (n--) { 
+        // TODO: Add checking stat structure for all files and printing like ls -al
         printf("%hhu\t%lu\t%hu\t%s\n", namelist[n]->d_type, namelist[n]->d_ino,namelist[n]->d_reclen, namelist[n]->d_name);
         free(namelist[n]);
     }
@@ -35,3 +36,21 @@ int main(void)
     exit(EXIT_SUCCESS);
 }
 
+/* 
+    ls -al
+
+total 16
+drwxrwxr-x 2 dev dev 4096 окт 19 23:26 .    
+drwxrwxr-x 5 dev dev 4096 окт  6 21:09 ..
+-rw-rw-r-- 1 dev dev 1103 окт 19 23:28 main.c
+-rw-rw-r-- 1 dev dev   69 окт  6 21:09 Makefile
+
+1. filetype
+2. permission flags in positive view
+3. count of links for file
+4. owner name
+5. owner group
+6. file size
+7. time of LastModification
+8. name
+*/
